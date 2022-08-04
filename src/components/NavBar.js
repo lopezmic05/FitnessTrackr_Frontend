0c70fceb9fdd34c918764e52f48a6c44ba7008e2
Michael Lopez <michael.94lopez@gmail.com>
2022-08-03 21:22:57 -0500

    building Routines api

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,7 @@ const NavBar = () => {
     <Link to="/">Home</Link> | {""}
         <Link to="Login">Login</Link> | {""}
         <Link to="profile">Profile</Link> | {""}
+        <Link to="routines">Routines</Link> | {""}
         <Link to="register">Register</Link>
       </nav>
     
