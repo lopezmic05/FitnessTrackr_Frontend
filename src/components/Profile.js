939f76c106782f2f70e2e51dfffe35b41aed3b47
Michael Lopez <michael.94lopez@gmail.com>
2022-08-05 13:49:19 -0500

    Got routines to show, styled routines with CSS

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
-import { getUser } from "../api";
+import { getUser, getUserRoutine } from "../api";
 
 const Profile = () => {
     const [info, setInfo] = useState ({})
+    const [userRoutine, setUserRoutines] = useState({})
     const getUserInfo = async () =>{
         try {
             const result = await getUser()
@@ -14,12 +15,29 @@ const Profile = () => {
             console.error(error)
         }
     }
+
+    const getRoutinesInfo = async () =>{
+        try {
+            const RoutinesResult = await getUserRoutine()
+            if(RoutinesResult){
+                console.log(RoutinesResult, "I am User Routine Result")
+                setUserRoutines(RoutinesResult)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
     useEffect(() =>{
         getUserInfo()
+        getRoutinesInfo()
     },[])
+    console.log(userRoutine, "THIS IS USER ROUTINE!!!!!!!!!!!!")
+
     return ( 
         <div>
             {info && info.username ?<h3>Hello {info.username}!</h3> : null}
+
+            <p>These are your routines:{userRoutine && userRoutine.length ?<h3> {userRoutine.username}!</h3> : null}</p>
         </div>
      );
 }
